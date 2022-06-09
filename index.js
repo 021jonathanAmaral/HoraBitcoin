f968a8a832472eebab1178a1146b7caf3750c226
Amon Silva <35241797+Silva-Amon@users.noreply.github.com>
2022-06-09 17:33:03 -0300

    Sugestão de melhoria

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
+
 const baseUrl = 'https://api.blockchair.com/bitcoin/stats';
-
-
+var blocks
+var difficulty
 
 function getElement(element) {
     return document.querySelector(element);
-  }
+}
 
 var init = {
     method: 'GET',
     blocos: 'blocks'
 }
 
-  function consultAPI(){
-      
-      fetch(baseUrl, init)
+function consultAPI() {
+
+    fetch(baseUrl, init)
         .then(response => response.json())
-        .then(data => console.log(data) )
-
-   
-  }
-  
-/*
- Site da API: https://blockchair.com/api/docs#link_001
-*/
-
-
- 
-  
-
+        .then(result => {
+            
+            console.log(result)
+            
+            console.log(result.data.blocks)
+            
+            console.log(result.data.difficulty)
+            
+            blocks = result.data.blocks
+            difficulty = result.data.difficulty
+        })
+        
+    }
+    // Colocar os console logs abaixo em uma função 
+    // e passar ela como parametro da função consultaAPI
+    // Exemplo: function consultaAPI(callback)
+    consultAPI();
+    console.log("blocks", blocks)
+    console.log("difficulty", difficulty)
